8bfbf7b29c08bc4d670076415f9a4df49455f262
Roman Bodnar <Roman.Bodnar.ShI.2023@lpnu.ua>
2025-03-21 21:06:35 +0200

    User page UI

---
+++ src/pages/UserPage/UserPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Profile from "../../components/UserInfo/Profile.tsx";
+
+const UserPage: React.FC = () => {
+
+    return (
+        <div>
+            <Profile/>
+        </div>
+    );
+};
+
+export default UserPage;
