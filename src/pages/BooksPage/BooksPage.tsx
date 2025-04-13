8ed8aa5a97c60f7d2a823e020a552439c34c6beb
Roman Bodnar <Roman.Bodnar.ShI.2023@lpnu.ua>
2025-04-13 22:58:06 +0300

    UI update, add individual book page.

--- src/pages/BooksPage/BooksPage.tsx
+++ src/pages/BooksPage/BooksPage.tsx
@@ -6,9 +6,11 @@ import { Book } from '../../interfaces/IBook';
 import BookCard from '../../components/BookCard/BookCard';
 import BooksFilters from '../../components/BooksFilters/BooksFilters';
 import { IBookQueryParams, useBooks } from '../../hooks/useBooks';
+import { useNavigate } from 'react-router-dom';
 
 const BooksPage: React.FC = () => {
     const [queryParams, setQueryParams] = useState<IBookQueryParams>({});
+    const navigate = useNavigate();
 
     const { data: books, isLoading, isError } = useBooks(queryParams);
 
@@ -16,6 +18,10 @@ const BooksPage: React.FC = () => {
     const handleFiltersChange = useCallback((params: IBookQueryParams) => {
         setQueryParams(params);
     }, []);
+
+    const handleBookClick = (bookId: number) => {
+        navigate(`/book/${bookId}`);
+    };
 
     if (isError) {
         message.error('Не вдалося завантажити список книг');
@@ -35,7 +41,7 @@ const BooksPage: React.FC = () => {
                     grid={{ gutter: 16, column: 4 }}
                     dataSource={books || []}
                     renderItem={(book: Book) => (
-                        <List.Item className={styles.listItem} key={book.id}>
+                        <List.Item className={styles.listItem} key={book.id} onClick={() => handleBookClick(book.id)}>
                             <BookCard book={book} />
                         </List.Item>
                     )}
